701d23bde945b27409c9a73c2980c62e023dbcb3
LETICIA SOUZA ALMEIDA <24122661@dsn127m1246729.cfp127aluno.local>
2025-03-20 17:21:17 -0300

    Alterações no JS e CSS

--- app.js
+++ app.js
@@ -6,30 +6,44 @@ const personagens = document.getElementById('galeria')
 
 // Função para encontrar o heroi digitado
 async function pesquisarInformacoes(nome){
-    const url = `https://superheroapi.com/api/896c3ae8a2e2d92b14388ca61a36127e/${nome}`
+    const url = `https://superheroapi.com/api/896c3ae8a2e2d92b14388ca61a36127e/search/${nome}`
 
     const response  = await fetch(url) // fetch -> faz requisições web (conversa com o back)
     const data      = await response.json() // chamar apenas o json
-    console.log(data)
 }
 
 // Função para criar os cards dentro da DIV 
 function criarCard(json){
-    const novoCard = document.createElement('div') // criando novo card
 
-    novoCard.innerHTML = `
-            
-    `
+    json.forEach(item => {
+        const card = document.createElement('div')
+        card.classList.add('character-card')
+        card.addEventListener('click', () => detalhesDoPersonagem(item))
 
+        // Cria a imagem do personagem
+        const img = document.createElement('img')
+        img.src = item.image || './imgs/placeholder.jpg'
+        img.alt = item.name
 
-    personagens.appendChild(novoCard)
+        // Cria o nome do personagem
+        const name = document.createElement('h3')
+        name.textContent = character.name
+    })
+
+    // const novoCard = document.createElement('div') // criando novo card
+    // novoCard.innerHTML = `
+    //     <h1>Legal</h1>
+    // `
+    // personagens.appendChild(novoCard)
 }
 
 async function preencherCard(){
     const valorDigitado = document.getElementById('barraPesquisa').value // Reconhece o valor digitado dentro do input 
-    const herois = await pesquisarInformacoes(heroi)
+    const herois = await pesquisarInformacoes(valorDigitado)
 
-    fotos.forEach (criarImagem)
+    const result = await criarCard()
+    
+    personagens.appendChild(result)
 }
 
 async function deletarTela(){
